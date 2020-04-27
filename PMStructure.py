5473222f1f1c479cef6a637fd11fa5590640b1f5
paul.twine@postgrad.manchester.ac.uk <p17992pt@hlogin1.pri.csf3.alces.network>
2020-04-27 15:29:12 +0100

    Updates after running job arrays

--- PMStructure.py
+++ PMStructure.py
@@ -14,6 +14,6 @@ pipeline.modifiers.append(om.PolyhedralTemplateMatchingModifier(rmsd_cutoff=fltR
 pipeline.compute()
 vp = Viewport(type = Viewport.Type.Top)
 vp.zoom_all()
-vp.render_image(filename=strFilename + '.png', size=(320, 240),  renderer=TachyonRenderer())
+vp.render_image(filename=strFilename + '.png', size=(320, 240),frame=n-1,  renderer=TachyonRenderer())
 #print(pipeline.output) #debug information
 io.export_file(pipeline, strFilename + 'PM', "lammps_dump", frame = n, columns = ['Particle Identifier','Position.X','Position.Y', 'Position.Z', 'Velocity.X', 'Velocity.Y','Velocity.Z','c_pe1','Structure Type', 'Volumetric Strain'])
