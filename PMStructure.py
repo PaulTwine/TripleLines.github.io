29224f2cd718dde386d2c1bbf75e5bfa6c2f9c8b
paul.twine@postgrad.manchester.ac.uk <p17992pt@hlogin2.pri.csf3.alces.network>
2020-09-01 22:08:33 +0100

    PMStructure updated

--- PMStructure.py
+++ PMStructure.py
@@ -3,7 +3,7 @@ import ovito.modifiers as om
 import ovito.data as oid
 import numpy as np
 import sys
-from ovito.vis import Viewport, TachyonRenderer
+#from ovito.vis import Viewport, TachyonRenderer
 strFilename = str(sys.argv[1]) #assumes "ovitos PMStructure.py strFilename  fltRMSD "command line 
 fltRMSD = float(sys.argv[2])
 pipeline = io.import_file(strFilename, multiple_frames= True)
@@ -12,8 +12,8 @@ pipeline.add_to_scene()
 pipeline.modifiers.append(om.ElasticStrainModifier(calculate_strain_tensors=False, lattice_constant = 4.05))
 pipeline.modifiers.append(om.PolyhedralTemplateMatchingModifier(rmsd_cutoff=fltRMSD,output_orientation =False)) #this needs to be last otherwise the structure types are not updated
 pipeline.compute()
-vp = Viewport(type = Viewport.Type.Top)
-vp.zoom_all()
-vp.render_image(filename=strFilename + '.png', size=(320, 240),frame=n-1,  renderer=TachyonRenderer())
+#vp = Viewport(type = Viewport.Type.Top)
+#vp.zoom_all()
+#vp.render_image(filename=strFilename + '.png', size=(320, 240),frame=n-1,  renderer=TachyonRenderer())
 #print(pipeline.output) #debug information
-io.export_file(pipeline, strFilename + 'PM', "lammps_dump", frame = n, columns = ['Particle Identifier','Position.X','Position.Y', 'Position.Z', 'Velocity.X', 'Velocity.Y','Velocity.Z','c_pe1','Structure Type', 'Volumetric Strain'])
+io.export_file(pipeline, strFilename , "lammps_dump", frame = n, columns = ['Particle Identifier','Position.X','Position.Y', 'Position.Z', 'Velocity.X', 'Velocity.Y','Velocity.Z','c_pe1','c_v[1]','Structure Type', 'Volumetric Strain'])
