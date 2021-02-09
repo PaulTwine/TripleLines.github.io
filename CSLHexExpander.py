40c51089923bafacbbd70c82da81f448791c456f
Paul Twine <paul.twine@postgrad.manchester.ac.uk>
2021-02-09 22:54:19 +0000

    Updates

--- CSLHexExpander.py
+++ CSLHexExpander.py
@@ -13,38 +13,38 @@ ax = fig.add_subplot(111, projection='3d')
 #intSigma = int(sys.argv[2])
 strDirectory = '../PythonLAMMPS/'
 arrAxis = np.array([1,1,1])
-intSigma = 3
+intSigma = 7
 objSigma = gl.SigmaCell(arrAxis,ld.FCCCell)
 objSigma.MakeCSLCell(intSigma)
-gf.CubicCSLGenerator(arrAxis, 5)
+print(gf.CubicCSLGenerator(arrAxis, 5))
 arrRotation = np.zeros(3)
 arrRotation[1:3] = objSigma.GetLatticeRotations()
 #arrRotation[0] = -arrRotation[1]
 arrSigmaBasis = objSigma.GetBasisVectors()
 a1 = 4.05 ##lattice parameter
 a2 = a1*np.sqrt(3) #periodic cell repeat multiple
-h= 1
+h= 3
 s = np.linalg.norm(arrSigmaBasis, axis=1)[0]
 z = a2*np.array([0,0,h])
-#intStart = np.ceil(max(4, s)).astype('int')
-intIncrement = np.ceil(1/s).astype('int')
+l = np.round(10/s,0).astype('int')
 fltAngle, arrVector = gf.FindRotationVectorAndAngle(arrAxis, np.array([0,0,1]))
 arr111BasisVectors = gf.RotatedBasisVectors(fltAngle, arrVector)
-for j in range(intIncrement,11+intIncrement,intIncrement):
-    l = j*s*intIncrement
-    arrHorizontalVector = l*a1*arrSigmaBasis[0]
-    arrDiagonalVector =  l*a1*arrSigmaBasis[1]
+arrHorizontalVector = l*a1*arrSigmaBasis[0]
+arrDiagonalVector =  l*a1*arrSigmaBasis[1]
+lstRotations = [[0,1,1],[0,2,2], [1,2,2], [0,1,2],[1,1,1]]
+for j in lstRotations:
     MySimulationCell = gl.SimulationCell(np.array([3*arrHorizontalVector,3*arrDiagonalVector, z]))
-    objHex1 = gl.ExtrudedRegularPolygon(l*a1*s, (h-0.1)*a2, 6, gf.RotateVectors(arrRotation[0],z,arr111BasisVectors), ld.FCCCell, np.array([a1,a1,a1]), arrHorizontalVector + arrDiagonalVector)
-    objHex2 = gl.ExtrudedRegularPolygon(l*a1*s, (h-0.1)*a2, 6, gf.RotateVectors(arrRotation[1],z,arr111BasisVectors), ld.FCCCell, np.array([a1,a1,a1]),np.zeros(3))
-    objHex3 = gl.ExtrudedRegularPolygon(l*a1*s, (h-0.1)*a2, 6, gf.RotateVectors(arrRotation[2],z, arr111BasisVectors), ld.FCCCell, np.array([a1,a1,a1]),-arrDiagonalVector+2*arrHorizontalVector)
+    objHex1 = gl.ExtrudedRegularPolygon(l*a1*s, (h-0.1)*a2, 6, gf.RotateVectors(arrRotation[j[0]],z,arr111BasisVectors), ld.FCCCell, np.array([a1,a1,a1]), arrHorizontalVector + arrDiagonalVector)
+    objHex2 = gl.ExtrudedRegularPolygon(l*a1*s, (h-0.1)*a2, 6, gf.RotateVectors(arrRotation[j[1]],z,arr111BasisVectors), ld.FCCCell, np.array([a1,a1,a1]),np.zeros(3))
+    objHex3 = gl.ExtrudedRegularPolygon(l*a1*s, (h-0.1)*a2, 6, gf.RotateVectors(arrRotation[j[2]],z, arr111BasisVectors), ld.FCCCell, np.array([a1,a1,a1]),-arrDiagonalVector+2*arrHorizontalVector)
+    objHex2.MakeManyVacancies(25, 3*a1)
     MySimulationCell.AddGrain(objHex1,'Hex1')
     MySimulationCell.AddGrain(objHex2,'Hex2')
     MySimulationCell.AddGrain(objHex3, 'Hex3')
     MySimulationCell.RemoveTooCloseAtoms(a1/(4*np.sqrt(2)))
-    MySimulationCell.WrapAllAtomsIntoSimulationCell()
-    MySimulationCell.SetFileHeader('Sigma ' + str(intSigma) + ' about ' + str(arrAxis) + ' with Hexagonal grains 1,2 and 3 with angle array ' + str(arrRotation) + ' and length multiple of ' + str(j))
-    strFileName = 'Hex' + '123' + 'Sigma' + str(intSigma) + 'length' + str(j)  + '.dat'
+    MySimulationCell.WrapAllPointsIntoSimulationCell()
+    MySimulationCell.SetFileHeader('Sigma ' + str(intSigma) + ' about ' + str(arrAxis) + ' with Hexagonal grains 1,2 and 3 with angle array ' + str(arrRotation))
+    strFileName = 'Hex' + str(j[0]+1) + str(j[1]+1) + str(j[2]+1) + 'Sigma' + str(intSigma) + '.dat'
     MySimulationCell.WriteLAMMPSDataFile(strDirectory + strFileName)
 
 
