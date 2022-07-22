cc19374971cdca7f2ba372443c4b25accdc07a31
PaulTwine <paul.twine@manchester.ac.uk>
2022-07-22 14:14:29 +0100

    updates for mobility test

---
+++ CSLTJCellDriven.py
@@ -0,0 +1,77 @@
+import numpy as np
+import LatticeDefinitions as ld
+import GeometryFunctions as gf
+import GeneralLattice as gl
+import LAMMPSTool as LT
+import sys
+import copy as cp
+import MiscFunctions
+
+
+strDirectory = str(sys.argv[1])
+intHeight = 1 #int(sys.argv[2]) #numbers of repeated CSL layers
+lstAxis = eval(str(sys.argv[2]))
+lstSigmaAxis = eval(str(sys.argv[3]))
+lstGrainPE = eval(str(sys.argv[5]))
+intTemp =  int(sys.argv[4])
+intRuns = 100000
+
+arrAxis = np.array(lstAxis)
+objCSL = gl.CSLTripleLine(arrAxis, ld.FCCCell) 
+arrCell = objCSL.FindTripleLineSigmaValues(75)
+fltTolerance = 0.5
+a = 4.05
+objCSL = gl.CSLTripleLine(arrAxis, ld.FCCCell) 
+arrCell = objCSL.FindTripleLineSigmaValues(75)
+intIndex = np.where(np.all(arrCell[:,:,0].astype('int')==lstSigmaAxis,axis=1))[0][0]
+arrCSL = arrCell[intIndex]
+objCSL.GetTJSigmaValue(arrCSL)
+objCSL.GetTJBasisVectors(intIndex,True)
+arrBasis = a*objCSL.GetSimulationCellBasis()
+arrMatrix = objCSL.GetRotationMatrix()
+intTJSigma = objCSL.GetTJSigmaValue(arrCSL)
+
+s = np.round(np.sqrt(2*10**5/(intHeight*np.linalg.det(arrBasis))))
+intRound = 10
+arrGrainBasis1 = np.round(objCSL.GetLatticeBasis(0),intRound) 
+arrGrainBasis2 = np.round(objCSL.GetLatticeBasis(2),intRound)
+arrGrainBasis3 = np.round(objCSL.GetLatticeBasis(1),intRound)
+
+lstGrains  = []
+lstGrains.append(arrGrainBasis1)
+lstGrains.append(arrGrainBasis2)
+lstGrains.append(arrGrainBasis3)
+
+
+arrOrientBases = np.round(np.append(np.matmul(a*ld.FCCPrimitive,lstGrains[lstGrainPE[0]]), np.matmul(a*ld.FCCPrimitive,lstGrains[lstGrainPE[1]]), axis=0),7)
+np.savetxt(strDirectory + 'Values.ori', arrOrientBases, delimiter=' ',fmt='%1.5f')
+
+
+
+arrFullCell = np.array([[4*s,0,0],[0,4*s,0],[0,0,intHeight]])
+arrSmallCell = np.array([[2*s,0,0],[0,2*s,0],[0,0,intHeight]])
+arrHorizontalCell = np.array([[4*s,0,0],[0,2*s,0],[0,0,intHeight]])
+arrVerticalCell = np.array([[2*s,0,0],[0,4*s,0],[0,0,intHeight]])
+
+arrFullBox = np.round(np.matmul(arrFullCell,arrBasis),intRound)
+arrSmallBox = np.round(np.matmul(arrSmallCell,arrBasis),intRound)
+arrHorizontalBox = np.round(np.matmul(arrHorizontalCell,arrBasis),intRound)
+arrVerticalBox = np.round(np.matmul(arrVerticalCell, arrBasis),intRound)
+
+objSimulationCell = gl.SimulationCell(arrFullBox)
+
+arrGrain1 = gl.ParallelopiedGrain(arrHorizontalBox,arrGrainBasis1,ld.FCCCell,a*np.ones(3), np.zeros(3))
+arrGrain2 = gl.ParallelopiedGrain(arrSmallBox,arrGrainBasis2,ld.FCCCell,a*np.ones(3), 0.5*arrFullBox[0]+0.5*arrFullBox[1])
+arrGrain3 = gl.ParallelopiedGrain(arrSmallBox,arrGrainBasis3,ld.FCCCell,a*np.ones(3),0.5*arrFullBox[1])
+
+fltNearestNeighbour = arrGrain1.GetNearestNeighbourDistance()
+fltE = fltTolerance*fltNearestNeighbour
+strFilename = 'TJ' + str(lstGrainPE[0]) + str(lstGrainPE[1])
+objSimulationCell.AddGrain(arrGrain1)
+objSimulationCell.AddGrain(arrGrain2)
+objSimulationCell.AddGrain(arrGrain3)
+
+objSimulationCell.MergeTooCloseAtoms(fltE,1)
+objSimulationCell.WriteLAMMPSDataFile(strDirectory + strFilename + '.dat')
+MiscFunctions.WriteDrivenTemplate(strDirectory,strFilename, intTemp, intRuns)
+
--- MiscFunctions.py
+++ MiscFunctions.py
@@ -61,6 +61,46 @@ def WriteAnnealTemplate(strDirectory: str, strFilename: str, intTemp: int, intRu
     fIn.write(strLAMMPS)
     fIn.close()
 
+
+def WriteRestartTemplate(strDirectory: str, strFilename: str, intTemp: int,intTimeStart:int, intRuns = 100000):
+    strLogFile =  strFilename + '.log'
+    str2MinDumpFile = '2Min*.dmp'
+    strDumpFile = '1Sim*.dmp'
+    strDatFile = strFilename + '.dat'
+    strLastMin = '2Min.lst'
+    strLAMMPS =''
+    strLAMMPS += 'units metal\n'
+    strLAMMPS += 'dimension 3\n'
+    strLAMMPS += 'boundary p p p\n'
+    strLAMMPS += 'atom_style atomic\n'
+    strLAMMPS += 'box tilt large\n'
+    strLAMMPS += 'read_data ' + strDatFile +'\n'
+    strLAMMPS += 'log ' + strLogFile +  '\n'
+    strLAMMPS += 'pair_style eam/alloy\n'
+    strLAMMPS += 'pair_coeff * * Al03.eam.alloy Al\n'
+    strLAMMPS += 'neighbor 0.3 bin\n'
+    strLAMMPS += 'neigh_modify delay 10\n'
+    strLAMMPS += 'thermo 100\n'
+    strLAMMPS += 'compute pe1 all pe/atom\n'
+    strLAMMPS += 'compute v all voronoi/atom\n'
+    strLAMMPS += 'compute pt all ptm/atom default 0.15 all\n'
+    strLAMMPS += 'compute st all stress/atom NULL virial\n'
+    strLAMMPS += 'reset_timestep ' + str(intTimeStart) + ' \n'
+    strLAMMPS += 'timestep 0.001\n'
+    strLAMMPS += 'dump 2 all custom 100 ' + strDumpFile + ' id x y z vx vy vz c_pe1 c_v[1] c_pt[1] c_pt[4] c_pt[5] c_pt[6] c_pt[7] c_st[1] c_st[2] c_st[3] c_st[4] c_st[5] c_st[6]\n'
+    strLAMMPS += 'velocity all create ' + str(intTemp) + ' 24577\n'
+    strLAMMPS += 'fix 2 all nvt temp ' + str(intTemp) + ' ' + str(intTemp) + ' $(100.0*dt)\n'
+    strLAMMPS += 'run ' +str(intRuns) + '\n'
+    strLAMMPS += 'unfix 2 \n'
+    strLAMMPS += 'undump 2 \n'
+    strLAMMPS += 'timestep 0.002\n'
+    strLAMMPS += 'dump 3 all custom 100 ' + str2MinDumpFile + ' id x y z vx vy vz c_pe1 c_v[1] c_pt[1] c_pt[4] c_pt[5] c_pt[6] c_pt[7] c_st[1] c_st[2] c_st[3] c_st[4] c_st[5] c_st[6]\n'
+    strLAMMPS += 'minimize 0.0 1.0e-6 10000 100000\n'
+    strLAMMPS += 'write_dump all custom ' + strLastMin + ' id x y z vx vy vz c_pe1 c_v[1] c_pt[1] c_pt[4] c_pt[5] c_pt[6] c_pt[7] c_st[1] c_st[2] c_st[3] c_st[4] c_st[5] c_st[6]\n'
+    fIn = open(strDirectory + strFilename + '.in', 'wt')
+    fIn.write(strLAMMPS)
+    fIn.close()
+
 def WriteMinTemplate(strDirectory: str, strFilename: str):
     strLogFile =  strFilename + '.log'
     str1MinDumpFile = strFilename + '*.dmp'
@@ -92,6 +132,61 @@ def WriteMinTemplate(strDirectory: str, strFilename: str):
     fIn = open(strDirectory + strFilename + '.in', 'wt')
     fIn.write(strLAMMPS)
     fIn.close()
+
+def WriteDrivenTemplate(strDirectory: str, strFilename: str, intTemp: int, intRuns = 100000):
+    strLogFile =  strFilename + '.log'
+    str1MinDumpFile = '1Min*.dmp'
+    str2MinDumpFile = '2Min*.dmp'
+    strDumpFile = '1Sim*.dmp'
+    strDatFile = strFilename + '.dat'
+    strFirstMin = '1Min.lst'
+    strLastMin = '2Min.lst'
+    strLAMMPS =''
+    strLAMMPS += 'units metal\n'
+    strLAMMPS += 'dimension 3\n'
+    strLAMMPS += 'boundary p p p\n'
+    strLAMMPS += 'atom_style atomic\n'
+    strLAMMPS += 'box tilt large\n'
+    strLAMMPS += 'read_data ' + strDatFile +'\n'
+    strLAMMPS += 'log ' + strLogFile +  '\n'
+    strLAMMPS += 'pair_style eam/alloy\n'
+    strLAMMPS += 'pair_coeff * * Al03.eam.alloy Al\n'
+    strLAMMPS += 'neighbor 0.3 bin\n'
+    strLAMMPS += 'neigh_modify delay 10\n'
+    strLAMMPS += 'thermo 100\n'
+    strLAMMPS += 'compute pe1 all pe/atom\n'
+    strLAMMPS += 'compute v all voronoi/atom\n'
+    strLAMMPS += 'compute pt all ptm/atom default 0.15 all\n'
+    strLAMMPS += 'compute st all stress/atom NULL virial\n'
+    strLAMMPS += 'dump 1 all custom 100 ' + str1MinDumpFile + ' id x y z vx vy vz c_pe1 c_v[1] c_pt[1] c_pt[4] c_pt[5] c_pt[6] c_pt[7] c_st[1] c_st[2] c_st[3] c_st[4] c_st[5] c_st[6]\n'
+    strLAMMPS += 'min_style fire\n'
+    strLAMMPS += 'timestep 0.002\n'
+    strLAMMPS += 'min_modify integrator verlet tmax 6.0 dmax 0.1\n'
+    strLAMMPS += 'minimize 0.0 1.0e-6 10000 100000\n'
+    strLAMMPS += 'write_dump all custom ' + strFirstMin + ' id x y z vx vy vz c_pe1 c_v[1] c_pt[1] c_pt[4] c_pt[5] c_pt[6] c_pt[7] c_st[1] c_st[2] c_st[3] c_st[4] c_st[5] c_st[6]\n'
+    strLAMMPS += 'undump 1 \n'
+    strLAMMPS += 'reset_timestep 0\n'
+    strLAMMPS += 'timestep 0.001\n'
+    strLAMMPS += 'fix 1 all orient/eco 0.08 0.25 4.05 Values.ori \n'
+    strLAMMPS += 'dump 2 all custom 100 ' + strDumpFile + ' id x y z vx vy vz c_pe1 c_v[1] c_pt[1] c_pt[4] c_pt[5] c_pt[6] c_pt[7] c_st[1] c_st[2] c_st[3] c_st[4] c_st[5] c_st[6]\n'
+    strLAMMPS += 'velocity all create ' + str(intTemp) + ' 24577\n'
+    strLAMMPS += 'fix 2 all nvt temp ' + str(intTemp) + ' ' + str(intTemp) + ' $(100.0*dt)\n'
+    strLAMMPS += 'run ' +str(intRuns) + '\n'
+    strLAMMPS += 'unfix 2 \n'
+    strLAMMPS += 'undump 2 \n'
+    strLAMMPS += 'unfix 1 \n'
+    strLAMMPS += 'timestep 0.002\n'
+    strLAMMPS += 'dump 3 all custom 100 ' + str2MinDumpFile + ' id x y z vx vy vz c_pe1 c_v[1] c_pt[1] c_pt[4] c_pt[5] c_pt[6] c_pt[7] c_st[1] c_st[2] c_st[3] c_st[4] c_st[5] c_st[6]\n'
+    strLAMMPS += 'minimize 0.0 1.0e-6 10000 100000\n'
+    strLAMMPS += 'write_dump all custom ' + strLastMin + ' id x y z vx vy vz c_pe1 c_v[1] c_pt[1] c_pt[4] c_pt[5] c_pt[6] c_pt[7] c_st[1] c_st[2] c_st[3] c_st[4] c_st[5] c_st[6]\n'
+    fIn = open(strDirectory + strFilename + '.in', 'wt')
+    fIn.write(strLAMMPS)
+    fIn.close()
+
+
+
+
+
 
 
 def ConfidenceInterval(arrValues: np.array, fltAlpha: float, blnInside = True)-> np.array:
