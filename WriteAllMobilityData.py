5dd6a496c292a3b8eb092d8bbb7aa66d42d0f8d4
PaulTwine <paul.twine@manchester.ac.uk>
2022-11-02 20:25:55 +0000

    Updates to detect grain collaspe and terminate while loop

--- WriteAllMobilityData.py
+++ WriteAllMobilityData.py
@@ -11,12 +11,12 @@ import copy as cp
 from scipy import spatial
 from scipy import optimize
 
-strDirectory = str(sys.argv[1])
-strType = str(sys.argv[2])
-intLow = int(sys.argv[3])
-intHigh = int(sys.argv[4])
-intStep = int(sys.argv[5])
-intReverse = int(sys.argv[6])
+strDirectory = strRoot = '/home/p17992pt/csf4_scratch/CSLTJMobility/Axis111/Sigma7_7_49/Temp450/u02/TJ/' #str(sys.argv[1])
+strType = 'TJ' #str(sys.argv[2])
+intLow = 45000 # int(sys.argv[3])
+intHigh = 50000 #int(sys.argv[4])
+intStep = 500 #int(sys.argv[5])
+intReverse = 0 #int(sys.argv[6])
 
 lstVolume = []
 lstTime = []
@@ -26,40 +26,55 @@ objAnalysis = objData.GetTimeStepByIndex(-1)
 intVColumn = objAnalysis.GetColumnIndex('c_v[1]')
 arrCellVectors = objAnalysis.GetCellVectors()
 fltCrossSection = np.linalg.norm(np.cross(arrCellVectors[0],arrCellVectors[2]))
-for t in range(intLow,intHigh+intStep,intStep):        
+t = intLow
+blnStop = False
+while t <= intHigh and not(blnStop): 
     objData = LT.LAMMPSData(strDirectory + '1Sim' + str(t) + '.dmp', 1, 4.05, LT.LAMMPSAnalysis3D)
     objAnalysis = objData.GetTimeStepByIndex(-1)
     arrIDs1 = objAnalysis.GetGrainAtomIDsByEcoOrient('f_1[2]',1)
-    objAnalysis.SetPeriodicGrain('1',arrIDs1, 25)
-    if strType == 'TJ':
+    if len(arrIDs1) > 0:
+        objAnalysis.SetPeriodicGrain('1',arrIDs1, 25)
+    else:
+        blnStop = True
+    if strType == 'TJ' and not(blnStop):
         arrIDs2 =  objAnalysis.GetGrainAtomIDsByEcoOrient('f_1[2]',-1)
-        arrIDs3 = objAnalysis.GetGrainAtomIDsByEcoOrient('f_2[2]',-1)
-        objAnalysis.SetPeriodicGrain('2',arrIDs2, 25)
-        objAnalysis.SetPeriodicGrain('3',arrIDs3, 25)
-        arrPoints12 = objAnalysis.FindDefectiveMesh('1','2',25)
-        arrPoints13 = objAnalysis.FindDefectiveMesh('1','3',25)
-        arrPoints23 = objAnalysis.FindDefectiveMesh('2','3',25)
-        if intReverse == 1:
-            arrVolumeIDs = np.append(arrIDs1, arrIDs2, axis=0)
+        if len(arrIDs2) > 0:
+            objAnalysis.SetPeriodicGrain('2',arrIDs2, 25)
         else: 
-            arrVolumeIDs = arrIDs1
-    else:
+            blnStop = True
+        if not(blnStop):
+            arrIDs3 = objAnalysis.GetGrainAtomIDsByEcoOrient('f_2[2]',-1)
+            if len(arrIDs3) > 0:
+                objAnalysis.SetPeriodicGrain('3',arrIDs3, 25)
+            else:
+                blnStop = True
+        if not(blnStop):
+            arrPoints12 = objAnalysis.FindDefectiveMesh('1','2',25)
+            arrPoints13 = objAnalysis.FindDefectiveMesh('1','3',25)
+            arrPoints23 = objAnalysis.FindDefectiveMesh('2','3',25)
+            if intReverse == 1:
+                arrVolumeIDs = np.append(arrIDs1, arrIDs2, axis=0)
+            else: 
+                arrVolumeIDs = arrIDs1
+    elif not(blnStop):
         arrIDs2 =  objAnalysis.GetGrainAtomIDsByEcoOrient('f_1[2]',-1)
-        objAnalysis.SetPeriodicGrain('2',arrIDs2, 10)
-        arrPoints12 = objAnalysis.FindDefectiveMesh('1','2',10)
-        arrVolumeIDs = arrIDs2
-    if len(arrVolumeIDs) > 0:
+        if len(arrIDs2) > 0:
+            objAnalysis.SetPeriodicGrain('2',arrIDs2, 10)
+            arrPoints12 = objAnalysis.FindDefectiveMesh('1','2',10)
+            arrVolumeIDs = arrIDs2
+        else:
+            blnStop = True
+    if not(blnStop):
         arrPoints = objAnalysis.GetAtomsByID(arrVolumeIDs)
-    else:
-        blnSplitGrains = True
-    if len(arrVolumeIDs) > 0:
         fltVolume = np.sum(objAnalysis.GetAtomsByID(arrVolumeIDs)[:,intVColumn])
     else: 
         fltVolume = 0
-    if len(arrPoints12) > 0:
+    if len(arrPoints12) > 0 and not(blnStop):
         arrPoints12 = objAnalysis.WrapVectorIntoSimulationBox(arrPoints12)
         np.savetxt(strDirectory + '/Mesh12' + strType + str(t) + '.txt', arrPoints12)
-    if strType == 'TJ':
+    else:
+        blnStop = True
+    if strType == 'TJ' and not(blnStop):
         if len(arrPoints13) > 0:
             arrPoints13 = objAnalysis.WrapVectorIntoSimulationBox(arrPoints13)
             np.savetxt(strDirectory + '/Mesh13' + strType + str(t) + '.txt', arrPoints13)
@@ -69,6 +84,7 @@ for t in range(intLow,intHigh+intStep,intStep):
     lstSpeed.append(fltVolume/fltCrossSection)
     lstVolume.append(fltVolume)
     lstTime.append(t)
+    t += intStep
 np.savetxt(strDirectory + '/Volume' + strType + '.txt', np.array([np.array(lstTime),np.array(lstVolume),np.array(lstSpeed)]))
 
 
