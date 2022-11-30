3da25c26db254a4cc3962581fa6feec1489c90e7
PaulTwine <paul.twine@postgrad.manchester.ac.uk>
2022-11-30 13:58:38 +0000

    Updates to correct error in random displacements

--- RandomPoints.py
+++ RandomPoints.py
@@ -1,17 +1,17 @@
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import LatticeDefinitions as ld
 import GeometryFunctions as gf
 import GeneralLattice as gl
 import LAMMPSTool as LT
 import copy as cp
 import sys
-#fig = plt.figure(figsize=plt.figaspect(1)) #Adjusts the aspect ratio and enlarges the figure (text does not enlarge)
-#ax = fig.gca(projection='3d')
+fig = plt.figure(figsize=plt.figaspect(1)) #Adjusts the aspect ratio and enlarges the figure (text does not enlarge)
+ax = fig.gca(projection='3d')
 a  = 4.05
-strDirectory = str(sys.argv[1])
-intSigma = int(sys.argv[2])
-lstAxis = eval(str(sys.argv[3]))
+strDirectory = ''# str(sys.argv[1])
+intSigma = 37 # int(sys.argv[2])
+lstAxis = [0,0,1] #eval(str(sys.argv[3]))
 intIncrements = 10# int(sys.argv[4])
 arrAxis = np.array(lstAxis)
 objSigma = gl.SigmaCell(arrAxis,ld.FCCCell)
@@ -19,8 +19,9 @@ objSigma.MakeCSLCell(intSigma)
 arrBasis = a*objSigma.GetBasisVectors()
 fltArea = np.linalg.norm(np.cross(arrBasis[1],arrBasis[2]))
 fltDistance = np.sqrt(fltArea/(2*intIncrements))
+#fltDistance = np.sqrt(fltArea/(np.pi*intIncrements))
 blnStop = False
-arrRandom = np.array([(0.5-np.random.ranf())*arrBasis[1]+(0.5-np.random.ranf())*arrBasis[2]])
+arrRandom = np.array([(np.random.ranf())*arrBasis[1]+(np.random.ranf())*arrBasis[2]])
 arrConstraints = gf.FindConstraintsFromBasisVectors(arrBasis)
 objPeriodicTree = gf.PeriodicWrapperKDTree(arrRandom, arrBasis,arrConstraints,fltDistance)
 i = 0
@@ -28,18 +29,20 @@ intPoints = 0
 while (blnStop == False and i < 100000):
     lstPoints = []
     if intPoints < 3:
-        arrStart = 0.5*(arrBasis[1]+arrBasis[2])
+        arrStart = 0.25*(arrBasis[1]+arrBasis[2])
     elif 3 <= intPoints < 5:
-        arrStart = 0.5*(arrBasis[1]-arrBasis[2])
+        arrStart = 0.25*arrBasis[1]+0.75*arrBasis[2]
     elif 3 <= intPoints < 5:
-        arrStart = 0.5*(-arrBasis[1]+arrBasis[2])
+        arrStart = 0.75*(arrBasis[1]+arrBasis[2])
     elif 3 <= intPoints < 5:
-        arrStart = -0.5*(arrBasis[1]+arrBasis[2])
+        arrStart = 0.75*arrBasis[1]+0.25*arrBasis[2]
     else:
-        arrStart = np.array([(0.5-np.random.ranf())*arrBasis[1]+(0.5-np.random.ranf())*arrBasis[2]])
-    arrRandom = 0.5*np.array([np.random.beta(2,2)*arrBasis[1]+np.random.beta(2,2)*arrBasis[2]]) +arrStart    
+        arrStart = np.array([(np.random.ranf())*arrBasis[1]+(np.random.ranf())*arrBasis[2]])
+    arrRandom = 0.5*np.array([np.random.beta(2,2)*arrBasis[1]+np.random.beta(2,2)*arrBasis[2]]) +arrStart
+    arrRandom = gf.WrapVectorIntoSimulationCell(arrBasis,arrRandom) #-0.5*(arrBasis[1]+arrBasis[2])    
     arrIndices = objPeriodicTree.Pquery_radius(arrRandom, fltDistance)[0]
     if len(np.unique(arrIndices)) == 1 and len(arrIndices[0]) == 0:
+        arrRandom = gf.WrapVectorIntoSimulationCell(arrBasis,arrRandom) 
         lstPoints.append(objPeriodicTree.GetOriginalPoints())
         lstPoints.append(arrRandom)
         arrPoints = np.vstack(lstPoints)
@@ -49,9 +52,10 @@ while (blnStop == False and i < 100000):
         else:
             objPeriodicTree = gf.PeriodicWrapperKDTree(arrPoints, arrBasis,arrConstraints,fltDistance)
     i +=1
-#ax.scatter(*tuple(zip(*objPeriodicTree.GetOriginalPoints())))
-#gf.EqualAxis3D(ax)
-#plt.show()
+ax.scatter(*tuple(zip(*objPeriodicTree.GetOriginalPoints())))
+gf.EqualAxis3D(ax)
+plt.show()
+arrPoints = arrPoints - 0.5*(arrBasis[1] + arrBasis[2])
 np.savetxt(strDirectory + 'AllRandomPoints',arrPoints,fmt='%f')
 
 for  k in range(intIncrements):
