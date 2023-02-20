b88327dc28c543bbe3dbb9db56fe6719208ef222
PaulTwine <paul.twine@postgrad.manchester.ac.uk>
2023-02-20 17:05:45 +0000

    new file for csf4

--- WriteDataByTJandGB.py
+++ WriteDataByTJandGB.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import AxisSigmaDeltaStores as AS
 strDir = str(sys.argv[1]) # '/home/paul/csf4_scratch/TJ/Axis001/TJSigma13/'
-strType = str(sys.argv[2]) # 'TJ'
+strType = str(sys.argv[2]) 
 lstAxis = eval(sys.argv[3])
 intSigma = int(sys.argv[4])
 intDir = int(sys.argv[5])
