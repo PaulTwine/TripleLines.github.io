fb05fa8f8c139a9d73cd9b85c19a77df51dd5a4a
PaulTwine <paul.twine@manchester.ac.uk>
2024-06-29 23:49:49 +0100

    Updates to include generic CSL finder

--- SmithNormalForm.py
+++ SmithNormalForm.py
@@ -1,11 +1,14 @@
+#%%
 import numpy as np
+import GeometryFunctions as gf
+#%%
 
 class SmithNormalForm(object):
     def __init__(self,inMatrix: np.array):
-        self.__OriginalMatrix = inMatrix
+        self.__OriginalMatrix = np.round(inMatrix)
         self.__intRows = np.shape(inMatrix)[0]
         self.__intColumns = np.shape(inMatrix)[1]
-        self.__DiagonalMatrix = inMatrix
+        self.__DiagonalMatrix = np.round(inMatrix)
         self.__Identity = np.identity(max([self.__intColumns,self.__intRows]))
         self.__LeftMatrix = np.copy(self.__Identity)
         self.__RightMatrix = np.copy(self.__Identity)
@@ -48,22 +51,22 @@ class SmithNormalForm(object):
         return arrMatrix
     def SwapColumns(self, i,j):
         arrSwap = self.SwapMatrix(i,j)
-        self.__DiagonalMatrix = np.matmul(self.__DiagonalMatrix,arrSwap)
-        self.__RightMatrix = np.matmul(self.__RightMatrix,arrSwap)
+        self.__DiagonalMatrix = np.round(np.matmul(self.__DiagonalMatrix,arrSwap))
+        self.__RightMatrix = np.round(np.matmul(self.__RightMatrix,arrSwap))
     def SwapRows(self, i,j):
         arrSwap = self.SwapMatrix(i,j)
-        self.__DiagonalMatrix = np.matmul(arrSwap,self.__DiagonalMatrix)
-        self.__LeftMatrix = np.matmul(arrSwap,self.__LeftMatrix)    
+        self.__DiagonalMatrix = np.round(np.matmul(arrSwap,self.__DiagonalMatrix))
+        self.__LeftMatrix =np.round(np.matmul(arrSwap,self.__LeftMatrix))    
     def InvertRow(self,i):
         arrInvert = np.copy(self.__Identity)
         arrInvert[i,i] = -1
-        self.__DiagonalMatrix = np.matmul(arrInvert,self.__DiagonalMatrix)
-        self.__LeftMatrix = np.matmul(arrInvert,self.__LeftMatrix)
+        self.__DiagonalMatrix = np.round(np.matmul(arrInvert,self.__DiagonalMatrix))
+        self.__LeftMatrix = np.round(np.matmul(arrInvert,self.__LeftMatrix))
     def InvertColumn(self,i):
         arrInvert = np.copy(self.__Identity)
         arrInvert[i,i] = -1
-        self.__DiagonalMatrix = np.matmul(self.__DiagonalMatrix,arrInvert)
-        self.__RightMatrix = np.matmul(self.__RightMatrix,arrInvert)    
+        self.__DiagonalMatrix = np.round(np.matmul(self.__DiagonalMatrix,arrInvert))
+        self.__RightMatrix = np.round(np.matmul(self.__RightMatrix,arrInvert))    
     def ReduceByFirstRow(self,intStep):
         arrOriginalRow = np.copy(self.__DiagonalMatrix[:,intStep])
         arrRow = np.zeros(len(arrOriginalRow))
@@ -75,8 +78,8 @@ class SmithNormalForm(object):
                     arrRow[i] = -np.round(arrOriginalRow[i]/arrOriginalRow[intStep])
         arrReduce = np.copy(self.__Identity)
         arrReduce[:,intStep] = arrRow
-        self.__DiagonalMatrix = np.matmul(arrReduce,self.__DiagonalMatrix)
-        self.__LeftMatrix = np.matmul(arrReduce,self.__LeftMatrix)
+        self.__DiagonalMatrix = np.round(np.matmul(arrReduce,self.__DiagonalMatrix))
+        self.__LeftMatrix = np.round(np.matmul(arrReduce,self.__LeftMatrix))
     def ReduceByFirstCol(self,intStep):
         arrOriginalCol = np.copy(self.__DiagonalMatrix[intStep,:])
         arrCol = np.zeros(len(arrOriginalCol))
@@ -88,8 +91,8 @@ class SmithNormalForm(object):
                     arrCol[i] = -np.round(arrOriginalCol[i]/arrOriginalCol[intStep])
         arrReduce = np.copy(self.__Identity)
         arrReduce[intStep,:] = arrCol
-        self.__DiagonalMatrix = np.matmul(self.__DiagonalMatrix,arrReduce)
-        self.__RightMatrix = np.matmul(self.__RightMatrix,arrReduce)
+        self.__DiagonalMatrix = np.round(np.matmul(self.__DiagonalMatrix,arrReduce))
+        self.__RightMatrix = np.round(np.matmul(self.__RightMatrix,arrReduce))
     def CheckZeros(self, i):
         blnReturn = False
         if i+1 < self.__intRows:
@@ -108,18 +111,22 @@ class SmithNormalForm(object):
             self.ReduceByFirstRow(i)
             arrSwap = self.FindPivot(self.__DiagonalMatrix[i:,i:]) + i
             self.SwapRows(arrSwap[0],i)
-            print(self.__DiagonalMatrix)
             self.ReduceByFirstCol(i)
-            print(self.__DiagonalMatrix)
             arrSwap = self.FindPivot(self.__DiagonalMatrix[i:,i:]) + i
             self.SwapColumns(arrSwap[1],i)
-            print(self.__DiagonalMatrix)
             if self.IsDiagonal(): #Check whether diagonal form is achieved
                 blnStop = True
             elif self.CheckZeros(i): # are the ith row and column all zero except at the diagonal  
                 i = i+1 #increment to look at the next submatrix
             n +=1 
-        arrSort = np.argsort(np.diag(self.__DiagonalMatrix))
+        arrDiagonal = np.copy(np.diag(self.__DiagonalMatrix))
+        k = 0
+        while k < len(arrDiagonal):
+            if arrDiagonal[k] < 0:
+                self.InvertRow(k)
+                arrDiagonal[k] = -arrDiagonal[k]
+            k += 1
+        arrSort = np.argsort(arrDiagonal)
         i = 0
         while i < len(arrSort): #put the diagonal entries in ascending
             if i != arrSort[i]:
@@ -141,6 +148,36 @@ class SmithNormalForm(object):
         if np.all(np.unique(arrMatrix)==0):
             blnReturn = True
         return blnReturn
+#%%
+class GeneralCSL(SmithNormalForm):
+    def __init__(self, inTransition,inBasis):
+        arrConjugate = np.matmul(np.linalg.inv(inBasis), np.matmul(inTransition,inBasis))
+        blnInt = False
+        n = 0
+        while not(blnInt) and n <50000:
+            n +=1
+            arrTest = n*arrConjugate
+            if np.all(np.round(arrTest,0) == np.around  (arrTest,10)):
+                blnInt=True
+        self.__ConjugateSigma = n
+        self.__IntegerTransition = np.round(n*arrConjugate)
+        SmithNormalForm.__init__(self,n*arrConjugate)
+        (self.__IntegerTransition)
+        self.__ConjugateTransition= arrConjugate
+        self.__Basis = inBasis
+    def GetConjugateSigma(self):
+        return self.__ConjugateSigma
+    def GetCSLPrimtiveCell(self):
+        if not(self.IsDiagonal()):
+            self.FindSmithNormal()
+        lstFactors = []
+        for j in range(3):
+            lstFactors.append(np.gcd(int(self.GetDiagonalMatrix()[j,j]),int(self.__ConjugateSigma)))
+        self.__Scaling = np.diag(lstFactors)
+    def GetScaling(self):
+        return self.__Scaling
+        
+#%%
 arrTest =     np.array([[4,3,0],[-3,4,0],[0,0,5]])
 print(np.linalg.inv(arrTest))
 #print(np.linalg.det(arrTest))
@@ -152,4 +189,47 @@ print(objSmith.FindSmithNormal())
 
 # gcd(a,b), p, q such that p*a+q*b = gcd(a,b)    
 print(np.matmul(np.linalg.inv(objSmith.GetLeftMatrix()),np.matmul(objSmith.GetDiagonalMatrix(),np.linalg.inv(objSmith.GetRightMatrix()))))
-                           
+
+#%%
+import GeneralLattice as gl
+import LatticeDefinitions as ld
+objSigma = gl.SigmaCell(np.array([1,1,1]),ld.FCCCell)
+objSigma.MakeCSLCell(49)
+# %%
+print(objSigma.GetCSLPrimitiveVectors()*2)
+
+# %%
+arrMatrix = objSigma.GetOriginalBasis()
+print(arrMatrix)
+print(str(arrMatrix[0,0].as_integer_ratio()))
+print(arrMatrix)
+# %%
+objSmith1 = SmithNormalForm(arrMatrix)
+objSmith1.FindSmithNormal().astype('int')
+np.linalg.inv(objSmith1.GetLeftMatrix())
+# %%
+arrCheck = np.matmul(np.linalg.inv(2*np.transpose(ld.FCCPrimitive)),arrMatrix,np.transpose(2*ld.FCCPrimitive))
+print(arrCheck)
+objSmith3 = SmithNormalForm(arrCheck)
+objSmith3.FindSmithNormal()
+#np.linalg.det(2*ld.FCCPrimitive)
+# %%
+objCon = GeneralCSL(arrMatrix, 2*np.transpose(ld.FCCPrimitive))
+objCon.FindSmithNormal()
+objCon.GetCSLPrimtiveCell()
+objCon.GetScaling()
+np.linalg.det(objCon.GetDiagonalMatrix()/49)
+# %%
+blnInt = False
+n = 0
+while not(blnInt) and n <50000:
+    n +=1
+    arrTest = n*arrCheck
+    if np.all(np.round(arrTest,0) == np.around(arrTest,10)):
+        blnInt=True
+        print(n)
+
+print(n)
+# %%
+print(arrTest)
+# %%
